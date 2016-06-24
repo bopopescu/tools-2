8be5df1fe18e745353e53a65544724291105ca42
Donghua.Lau <me@liudonghua.net>
2016-06-24 13:37:26 +0800

    C++11 thread sample

--- codes/cpp11/thread.cpp
+++ codes/cpp11/thread.cpp
@@ -2,33 +2,59 @@
 #include <thread>
 #include <unistd.h>
 
-class CThread{
-	CThread():mThread(run, NULL){}
-	CThread(void* argv):mThread(run, argv){}
-	~CThread(){
+class CThread
+{
+public:
+	//CThread():mThread(&CThread::start, this){} // don't know why can't define like this.
+	CThread(void *arg):mThread(&CThread::start, this, arg){}
+	virtual ~CThread(){
 		mThread.join();
 	}
 
-public:
-	virtual void run(CThread *pThread, void *argv){
-		this.argv = argv;
+	virtual void run(void *arg){
+		argv = arg;
 		std::cout << "CThread start running" << std::endl;
-		int n = 0;
-		while(n > 5){
+		int n = *(int*)argv;
+		while(n <= 5){
 			std::cout << "n = "  << n << std::endl;
 			n++;
 			sleep(1);
 		}
 	}
-	virtual bool start();
+	static bool start(void *thread_obj, void *arg){
+		CThread *pThread = (CThread *)thread_obj;
+		pThread->run(arg);
+	}
+	//static bool start(void *thread_obj){
+	//	CThread::start(thread_obj, 0);
+	//}
 
-private:
+protected:
 	void *argv;
 	std::thread mThread;
 
 };
 
+/*
+class MyThread : public CThread
+{
+public:
+	//MyThread(void *arg){}
+	CThread(void *arg):mThread(&CThread::start, this, arg){}
+	void run(void *arg)
+	{
+		std::cout << "I want do nothing." << std::endl;
+	}
+};
+*/
+
 int main()
 {
-	CThread mthd();
+	int n = 2;
+	//CThread mthd1();
+	CThread mthd2(&n);
+
+	n = 3;
+	MyThread mthd3(&n);
 }
+
